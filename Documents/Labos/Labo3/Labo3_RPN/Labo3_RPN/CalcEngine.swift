d4263b0a39e5556025664352ca3a6a859687ae21
Ilona Defevere <ilonadefevere@iMacI.local>
2025-10-15 10:37:09 +0200

    rest labo 3: uitbreiding button, commentaar toegevoegd

--- Documents/Labos/Labo3/Labo3_RPN/Labo3_RPN/CalcEngine.swift
+++ Documents/Labos/Labo3/Labo3_RPN/Labo3_RPN/CalcEngine.swift
@@ -10,8 +10,10 @@ import Foundation
 @Observable class CalcEngine{
     var result: String = ""
     var stack = [Double]()
+    //enteredNumbers omdat stack wordt geupdate met het resultaat van de berekening
     var enteredNumbers = [Double]()
     
+    //getal aan stack toevoegen
     func addToStack (getal: Double) {
         stack.append(getal)
         enteredNumbers.append(getal)
@@ -20,6 +22,7 @@ import Foundation
     func calculateResult (operand: String) {
         //lengte controleren
         if stack.count >= 2 {
+            //via poplast de waarden weghalen en in een variabele steken
             var laatste: Double = Double(stack.popLast()!)
             var voorlaatste: Double = Double(stack.popLast()!)
             
@@ -35,14 +38,18 @@ import Foundation
                 resultCalc = laatste / voorlaatste
             }
             
+            //de nieuwe berekening moet vooraan komen, dus daarvan '=' met op het einde '+ result'
             result = String(resultCalc) + "\n" + operand + "\n" + result
             addToStack(getal: resultCalc)
+            //resultaat moest niet toegevoegd worden, dus hier pop ik hem
             enteredNumbers.popLast()
         } else {
+            //errrormelding indien er niet genoeg waarden in de stack zitten voor een berekening te doen (stack.count >= 2)
             result = "Not enough operands..." + "\n" + operand + "\n" + result
         }
     }
     
+    //twee aparte functies omdat soms enkel de tekst moet gecleart worden
     func clearResult () {
         result = ""
     }
@@ -50,6 +57,7 @@ import Foundation
     func clearStack () {
         clearResult()
         stack = []
+        enteredNumbers = []
     }
     
     func showResult () -> (String) {
--- Documents/Labos/Labo3/Labo3_RPN/Labo3_RPN/CalculateView.swift
+++ Documents/Labos/Labo3/Labo3_RPN/Labo3_RPN/CalculateView.swift
@@ -15,17 +15,24 @@ import SwiftUI
 
 struct CalculateView: View {
     @State var calcEngine = CalcEngine()
+    //variabele resultText die resultaat tekst bijhoudt
     @State var resultText = ""
+    //variabele enteredNumber omdat 321 1 nummer blijft
+    //dan bij het doorgeven naar calcEngine casten naar Double
     @State var enteredNumber: String = ""
     var body: some View {
+        //Hstack met TextEditor
         HStack {
             VStack {
+                //tekst als constant, met resultText als waarde
                 TextEditor(text: .constant(resultText))
                     .frame(width: 200, height: 250)
+                //disabled zodat je er niet in kan typen
                     .disabled(true)
             }
             Grid {
                 GridRow {
+                    //foreach-loop om de number buttons te maken
                     ForEach(7..<10) {
                         number in
                         Button("\(number)") {
@@ -33,14 +40,8 @@ struct CalculateView: View {
                         }
                         .colorInvert()
                     }
-                    Button("/") {
-                        if enteredNumber != "" {
-                            calcEngine.addToStack(getal: Double(enteredNumber)!)
-                            enteredNumber = ""
-                        }
-                        calcEngine.calculateResult(operand: "/")
-                        resultText = calcEngine.showResult()
-                    }
+                    //calcButton (/, *, +, -) via klasse CalcButton
+                    CalcButton(operand: "/", calcEngine: $calcEngine, resultText: $resultText, enteredNumber: $enteredNumber)
                 }
                 GridRow {
                     ForEach(4..<7) {
@@ -48,16 +49,10 @@ struct CalculateView: View {
                         Button("\(number)") {
                             enteredNumber += String(number)
                         }
+                        //colorinvert voor zwarte button met witte tekst
                         .colorInvert()
                     }
-                    Button("*") {
-                        if enteredNumber != "" {
-                            calcEngine.addToStack(getal: Double(enteredNumber)!)
-                            enteredNumber = ""
-                        }
-                        calcEngine.calculateResult(operand: "*")
-                        resultText = calcEngine.showResult()
-                    }
+                    CalcButton(operand: "*", calcEngine: $calcEngine, resultText: $resultText, enteredNumber: $enteredNumber)
                 }
                 GridRow {
                     ForEach(1..<4) {
@@ -67,48 +62,50 @@ struct CalculateView: View {
                         }
                         .colorInvert()
                     }
-                    Button("-") {
-                        if enteredNumber != "" {
-                            calcEngine.addToStack(getal: Double(enteredNumber)!)
-                            enteredNumber = ""
-                        }
-                        calcEngine.calculateResult(operand: "-")
-                        resultText = calcEngine.showResult()
-                    }
+                    CalcButton(operand: "-", calcEngine: $calcEngine, resultText: $resultText, enteredNumber: $enteredNumber)
                 }
                 GridRow {
                     Button("0") {
                         enteredNumber += "0"
                     }
                     .colorInvert()
+                    //lege tekstvelden voor ruimte tussen button 0 en + button
                     Text("")
                     Text("")
-                    Button("+") {
-                        if enteredNumber != "" {
-                            calcEngine.addToStack(getal: Double(enteredNumber)!)
-                            enteredNumber = ""
-                        }
-                        calcEngine.calculateResult(operand: "+")
-                        resultText = calcEngine.showResult()
-                    }
+                    //oorspronkelijk:
+//                    Button("+") {
+//                        if enteredNumber != "" {
+//                            calcEngine.addToStack(getal: Double(enteredNumber)!)
+//                            enteredNumber = ""
+//                        }
+//                        calcEngine.calculateResult(operand: "+")
+//                        resultText = calcEngine.showResult()
+//                    }
+                    CalcButton(operand: "+", calcEngine: $calcEngine, resultText: $resultText, enteredNumber: $enteredNumber)
                 }
                 GridRow {
+                    //button voor te clearen
                     Button("Clear") {
                         calcEngine.clearStack()
                         resultText = ""
                         enteredNumber = ""
                     }
+                    //button clear en enter hebben lengte van 2 gewone buttons in de grid
                     .gridCellColumns(2)
 
+                    //enter button voor aan stack getal toe te voegen
                     Button("Enter") {
-                        calcEngine.addToStack(getal: Double(enteredNumber)!)
-                        enteredNumber = ""
+                        if(enteredNumber != "") {
+                            calcEngine.addToStack(getal: Double(enteredNumber)!)
+                            enteredNumber = ""
+                        }
                     }
                     .gridCellColumns(2)
                 }
             }
         }
         HStack {
+            //resultText updaten
             Button("Show Stack") {
                 resultText = calcEngine.showResult()
             }
